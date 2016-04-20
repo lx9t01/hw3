26787b3ae70daae569bb47292e1b12c7d301d2ae
lx9t01 <alex.xuhaixiang@gmail.com>
2016-04-19 21:16:59 -0700

    final

--- set3_problem_set/fft_convolve_cuda.cu
+++ set3_problem_set/fft_convolve_cuda.cu
@@ -113,22 +113,40 @@ cudaMaximumKernel(cufftComplex *out_data, float *max_abs_val,
 */
     __shared__ float data[1024];
     unsigned int i = threadIdx.x + blockDim.x * blockIdx.x;
-
+    const int last = padded_length % 1024;
     while (i < padded_length) {
-        if (i == 100) printf("%d max\n", &i);
-        data[threadIdx.x] = out_data[i].x;
-        __syncthreads();
-        
-        for (int j = 9; j >= 0; j--) {
-            int bias = 1 << j;
-            while (threadIdx.x < bias) {
-                data[threadIdx.x] = (fabs(data[threadIdx.x])>fabs(data[threadIdx.x + bias]))? \
+        if (i >= padded_length - last) {
+            data[thread_index.x] = out_data[i].x;
+            __syncthreads();
+
+            while(last > 1) {
+                int bias = last / 2;
+                while (threadIdx.x < bias) {
+                    data[threadIdx.x] = (fabs(data[threadIdx.x])>fabs(data[threadIdx.x + bias]))? \
                             data[threadIdx.x]:data[threadIdx.x + bias];
-                __syncthreads();
+                    __syncthreads();
+                }
+                last /= 2;
             }
-        }
-        atomicMax(max_abs_val, fabs(data[0]));
-        i += blockDim.x * gridDim.x;
+            atomicMax(max_abs_val, fabs(data[0]));
+            i += blockDim.x * gridDim.x;
+
+        } else {
+            if (i == 100) printf("%d max\n", &i);
+            data[threadIdx.x] = out_data[i].x;
+            __syncthreads();
+
+            for (int j = 9; j >= 0; j--) {
+                int bias = 1 << j;
+                while (threadIdx.x < bias) {
+                    data[threadIdx.x] = (fabs(data[threadIdx.x])>fabs(data[threadIdx.x + bias]))? \
+                            data[threadIdx.x]:data[threadIdx.x + bias];
+                    __syncthreads();
+                }
+            }
+            atomicMax(max_abs_val, fabs(data[0]));
+            i += blockDim.x * gridDim.x;
+        }  
     }
 }
 
